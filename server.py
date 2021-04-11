96e31d3783024a8163b74a4af90f4de406f3a87a
Martin Eldredge <martin.eldredge1@gmail.com>
2021-04-11 17:49:44 -0600

    add basic socket functionality

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+from _thread import *
+import sys
+
+HOST = ""
+PORT = 5050
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+print("Server started")
+print("Waiting for a connection to the server...")
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
+
+    # Start a new thread after this to process signals
