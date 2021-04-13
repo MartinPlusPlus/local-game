84dabca9bee3e23d9928b36d3f8ec627e9efc709
Martin Eldredge <martin.eldredge1@gmail.com>
2021-04-12 18:30:25 -0600

    send a random number over WebSocket

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
 import socket
 import sys
+import random
 
-HOST = "localhost" # Temporary
+random.seed()
+
+HOST = "192.168.0.110" # Temporary
 PORT = 5050
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(socket.getaddrinfo(HOST, PORT, socket.AF_INET, socket.SOCK_STREAM))
-s.connect(("127.0.0.1", 5050))
+s.connect((HOST, 5050))
 
-s.sendall(b"Hello, World!")
+cool_num = bytes([random.randrange(48, 58)])
+
+s.sendall(cool_num)
